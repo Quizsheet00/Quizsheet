ea97d2699b11481e4acb985a9b77f36efd741900
Quizsheet00 <alexsmith.k787@gmail.com>
2026-08-21 13:32:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+async function login() {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email === "" || password === "") {
+    alert("Please enter email and password");
+    return;
+  }
+
+  const { data, error } =
+    await supabaseClient.auth.signInWithPassword({
+      email: email,
+      password: password
+    });
+
+  if (error) {
+    alert("Wrong email or password");
+    return;
+  }
+
+  alert("Login successful!");
+
+  window.location.href = "dashboard.html";
+}
