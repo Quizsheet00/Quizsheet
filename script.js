b3f81b642d5ed9f2a44b4c5c6cea6f393290773a
Quizsheet00 <alexsmith.k787@gmail.com>
2026-08-21 13:14:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = https://hjdadrztyuysxntmxolh.supabase.co;
+const SUPABASE_KEY = sb_publishable_bf6orgWbQgwjycYWgLmvUg_Z8s6mGV3
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
